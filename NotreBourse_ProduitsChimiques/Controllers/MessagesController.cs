065730c7ffda23a068368bcc19e6f2dc59bb99c5
Mtir Manari <mtir158@gmail.com>
2019-03-12 13:26:53 +0100

    efefdf

---
+++ NotreBourse_ProduitsChimiques/Controllers/MessagesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotreBourse_ProduitsChimiques.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Messages
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult AddMessage()
+        {
+            return View();
+        }
+    }
+}
